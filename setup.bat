978aa235b3e2182a7a6c91b57d762cea04f8762c
Calculator <hjass2865@gmail.com>
2026-07-16 10:55:27 +0800

    Delete setup.bat

--- setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Cleaning old builds...
-rd /s /q node_modules package-lock.json
-echo Installing dependencies...
-call npm install
-echo --------------------------------------
-echo ✅ Windows Setup Complete!
-echo --------------------------------------
-pause
